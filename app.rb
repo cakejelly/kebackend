bbd8e6a070a2a28c5bee2d555371d5d72d5ee963
Patrick Oberherr <patrick.oberherr@gmail.com>
2017-01-09 13:40:27 +0100

    Add webhook recieving stuff

--- app.rb
+++ app.rb
@@ -1,3 +1,16 @@
+require 'json'
+
 post '/' do
   'Hello world'
+end
+
+def extract_params(content)
+  # Extracts the content we need
+  # for fetching
+end
+
+post '/kebabfetcher' do
+  request.body.rewind  # in case someone already read it
+  data = JSON.parse request.body.read
+  puts JSON.pretty_generate(data)
 end
